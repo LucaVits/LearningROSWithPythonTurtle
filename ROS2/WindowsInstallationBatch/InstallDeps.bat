1d85181040b26c24a0df3cf80f730b024362e15e
Joe Pizzimenti <jpizzi243@gmail.com>
2025-02-20 13:51:21 -0500

    added many packages to installation script, added execution policy check for chocolatey install

--- ROS2/WindowsInstallationBatch/InstallDeps.bat
+++ ROS2/WindowsInstallationBatch/InstallDeps.bat
@@ -4,11 +4,29 @@ cd "%userprofile%/Downloads"
 REM USER SHOULD BE INSTRUCTED TO INSTALL APP INSTALLER FROM WINDOWS STORE BEFORE RUNNING THIS SCRIPT
 
 
-REM 7zip
+REM Install Chocolatey
+::Check user policy before attempting chocolatey install
+::for loop to parse through execution policy var
+for /f "delims=" %%E in ('powershell -Command "Get-ExecutionPolicy"') do set POLICY=%%E
+
+if /I "%POLICY%"=="Restricted" (
+    powershell -Command "Set-ExecutionPolicy AllSigned -Force"
+    echo Execution policy changed to AllSigned for Chocolatey Install
+)
+else (
+    echo Execution policy is already set to %POLICY%. Proceeding to Chocolatey Install
+)
+::Run offical chocolatey install command
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+REM INSTALL 7zip
 winget install --id 7zip.7zip
 
-REM PYTHON (official docs ask for this specific version)
+
+REM PYTHON
 winget install --id Python.Python.3.8 -v 3.8.3
+
 
 REM VISUAL C++ LIBS
 winget install --id Microsoft.VCRedist.2013.x64
@@ -16,3 +34,80 @@ winget install --id Microsoft.VCRedist.2013.x86
 winget install --id Microsoft.VCRedist.2015+.x64
 winget install --id Microsoft.VCRedist.2015+.x86
 
+
+REM OPENSSL
+choco install -y openssl --version 1.1.1.2100
+setx /m OPENSSL_CONF "C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
+
+
+REM OPENSSL WITHOUT CHOCOLATEY (DO NOT USE)
+REM firedaemon is the only distributor of any v1.1.1 openssl window binaries INCLUDING ./lib/ and ./include/ that I could find
+REM feel free to replace URL with an official/better binary of OpenSSLin the future if you want to replace chocolatey entirely
+::mkdir "C:\Program Files\OpenSSL-Win64"
+::set "opensslurl=https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-1.1.1u.zip"
+::set "zipfile=openssl-1.1.1u.zip"
+::set "destination=C:\Program Files\OpenSSL-Win64"
+::curl -o "%zipfile%" "%opensslurl%"
+::if not exist "%zipfile%" (
+::   echo Download failed. URL Unreachable. Exiting...
+::    exit /b 1
+::)
+::7z x "%zipfile%" -o"%USERPROFILE%\Downloads\openssl-extracted"
+::if not exist "%USERPROFILE%\Downloads\openssl-extracted" (
+::    echo Extraction failed. Exiting...
+::    exit /b 1
+::)
+::xcopy "%USERPROFILE%\Downloads\openssl-extracted\openssl-1.1\x64\*" "%destination%" /E /I /Y
+::if not exist "%destination%" (
+::    echo Copy operation failed. Exiting...
+::    exit /b 1
+::)
+::setx /m OPENSSL_CONF "C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
+::echo OpenSSL installation completed successfully.
+
+
+
+REM VISUAL STUDIO
+set "vsurl=https://aka.ms/vs/16/release/vs_community.exe"
+set "vsfilename=program-installer.exe"
+
+curl -o "%vsfilename%" "%vsurl%"
+if not exist "%vsfilename%" (
+    echo Download failed. URL unreachable. Exiting...
+    exit /b 1
+)
+
+REM MUST MUST MUST Prompt user to uncheck "C++ CMake tools for Windows"
+start vs_community.exe
+
+REM to-do, wait for user to finish installing
+echo Installation and setup completed.
+
+
+
+REM Install OpenCV
+set "opencvurl=https://github.com/ros2/ros2/releases/download/opencv-archives/opencv-3.4.6-vc16.VS2019.zip”
+set "opencvzipfile=opencv-3.4.6-vc16.VS2019.zip"
+
+
+curl -o "%opencvzipfile%" "%opencvurl%"
+if not exist "%opencvzipfile%" (
+    echo Download failed. Exiting...
+    exit /b 1
+)
+
+7z x "%opencvzipfile%" -o"%USERPROFILE%\Downloads"
+if not exist "%USERPROFILE%\Downloads" (
+    echo Extraction failed. Exiting...
+    exit /b 1
+)
+
+xcopy "%USERPROFILE%\Downloads\opencv\*" "C:\" /E /I /Y
+if not exist "C:\opencv" (
+    echo Copy operation failed. Exiting...
+    exit /b 1
+)
+
+
+REM CMake
+winget install --id Kitware.CMake
--- ROS2/WindowsInstallationBatch/WindowsInstallInstructions.md
+++ ROS2/WindowsInstallationBatch/WindowsInstallInstructions.md
@@ -2,11 +2,21 @@ ROS2 Native Windows Install Guide (WIP)
 =================================
 To Install ROS2 on Windows, official documentation gives installation instructions using Chocolatey, a 3rd party windows package manager.
 
-This guide exists to install ROS2 distributions without Chocolatey, and relying off Microsoft's 1st party Windows package manager: winget. This way, all dependencies are installed directly on the user's system without the needed for a sandboxed package manager.
+This guide exists to install ROS2 distributions with some Chocolatey packages & some native packages by relying off Microsoft's 1st party Windows package manager: winget. The script should prompt users when their interaction is needed.
 
 To start installing dependencies for ROS2 without Chocolatey, run the InstallDeps.bat file found in this repo. This will automatically install some dependencies with winget, and others by downloading binaries files from various sources and installing them in various places. The programs installed are as follows:
+- Chocolatey [from website]
+- 7-zip (latest) [winget]
 - Python 3.8 (3.8.3) [winget]
 - Microsoft Visual C++ 2013 Redistributable (x64 & x86) (latest) [winget]
 - Microsoft Visual C++ 2015-2022 Redistributable (x64 & x86) (latest) [winget]
+- OpenSSL (1.1.1.2100) [choco]
+- Visual Studio Community (2019) [microsoft website]
+NOTE - Must check the following boxes:
+https://docs.ros.org/en/humble/_images/windows-vs-studio-install.png
+- OpenCV (3.4.6) [ROS2 github archive]
+- Cmake (latest) [winget]
+
+
 
 After this process is complete, your machine is now ready to run a ROS2 installation. This can be found 
