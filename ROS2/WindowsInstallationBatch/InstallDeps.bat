b9bc3955a6272e0f92747f3e85147004896d399b
Joe Pizzimenti <jpizzi243@gmail.com>
2025-03-18 17:33:40 -0400

    Add error checks for curl/7z, fix pip install, properly invoked powershell when necessary.

--- ROS2/WindowsInstallationBatch/InstallDeps.bat
+++ ROS2/WindowsInstallationBatch/InstallDeps.bat
@@ -1,12 +1,30 @@
 @echo off
+setlocal
+
+REM set user to downloads folder to avoid permission issues in System32
 cd "%userprofile%/Downloads"
 
-REM USER SHOULD BE INSTRUCTED TO INSTALL APP INSTALLER FROM WINDOWS STORE BEFORE RUNNING THIS SCRIPT
+REM use absolute path for curl. Not sure why curl -o can't just be called on it's own
+set "curlPath=%SystemRoot%\System32\curl.exe"
+:: Check if system's curl executable exists here
+if not exist "%curlPath%" (
+    echo curl not found at %zipPath%. Check if you are on Windows 10/11. Exiting...
+    exit /b 1
+REM same with 7zip. This is simpler than adding to PATH & forcing user to reboot:
+set "zipPath=%ProgramFiles%\7-Zip\7z.exe"
+:: Check if 7-Zip executable exists here
+if not exist "%zipPath%" (
+    echo 7-Zip not found at %zipPath%. Exiting...
+    exit /b 1
+)
+
+REM NEEDS TO BE INSTRUCTED TO INSTALL APP INSTALLER FROM WINDOWS STORE BEFORE RUNNING THIS SCRIPT
 
 
 REM Install Chocolatey
 ::Check user policy before attempting chocolatey install
-::for loop to parse through execution policy var (not working yet)
+::for loop to parse through execution policy var (not working)
+::Should not be required, powershell script for choco now has built in execution policy Bypass
 for /f "delims=" %%i in ('powershell -Command "Get-ExecutionPolicy"') do set POLICY=%%i
 
 if /I "%POLICY%"=="Restricted" (
@@ -14,7 +32,7 @@ if /I "%POLICY%"=="Restricted" (
 		::DO NOT USE ABOVE! Unsafe and results in permission issues in this script
     echo Execution policy is restricted. Setting policy to be bypassed for the install of Chocolatey
 ) else (
-    echo Execution policy is already set to %POLICY%. Proceeding to Chocolatey Install
+    echo Execution policy is currently set to %POLICY%. Proceeding to Chocolatey Install...
 )
 ::Run offical chocolatey install command
 powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
@@ -27,9 +45,8 @@ winget install --id 7zip.7zip
 
 REM PYTHON
 winget install --id Python.Python.3.8 -v 3.8.3
-
-python -3.8.3 -m pip install -U pip setuptools==59.6.0
-python -3.8.3 -m pip install -U catkin_pkg cryptography empy importlib-metadata lark==1.1.1 lxml matplotlib netifaces numpy opencv-python PyQt5 pillow psutil pycairo pydot pyparsing==2.4.7 pyyaml rosdistro
+py -3.8.3 -m pip install -U pip setuptools==59.6.0
+py -3.8.3 -m pip install -U catkin_pkg cryptography empy importlib-metadata lark==1.1.1 lxml matplotlib netifaces numpy opencv-python PyQt5 pillow psutil pycairo pydot pyparsing==2.4.7 pyyaml rosdistro
 
 
 REM VISUAL C++ LIBS
@@ -44,33 +61,6 @@ choco install -y openssl --version 1.1.1.2100
 setx /m OPENSSL_CONF "C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
 
 
-REM OPENSSL WITHOUT CHOCOLATEY (DO NOT USE)
-REM firedaemon is the only distributor of any v1.1.1 openssl window binaries INCLUDING ./lib/ and ./include/ that I could find
-REM feel free to replace URL with an official/better binary of OpenSSLin the future if you want to replace chocolatey entirely
-::mkdir "C:\Program Files\OpenSSL-Win64"
-::set "opensslurl=https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-1.1.1u.zip"
-::set "zipfile=openssl-1.1.1u.zip"
-::set "destination=C:\Program Files\OpenSSL-Win64"
-::curl -o "%zipfile%" "%opensslurl%"
-::if not exist "%zipfile%" (
-::   echo Download failed. URL Unreachable. Exiting...
-::    exit /b 1
-::)
-::7z x "%zipfile%" -o"%USERPROFILE%\Downloads\openssl-extracted"
-::if not exist "%USERPROFILE%\Downloads\openssl-extracted" (
-::    echo Extraction failed. Exiting...
-::    exit /b 1
-::)
-::xcopy "%USERPROFILE%\Downloads\openssl-extracted\openssl-1.1\x64\*" "%destination%" /E /I /Y
-::if not exist "%destination%" (
-::    echo Copy operation failed. Exiting...
-::    exit /b 1
-::)
-::setx /m OPENSSL_CONF "C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
-::echo OpenSSL installation completed successfully.
-
-
-
 REM VISUAL STUDIO
 set "vsurl=https://aka.ms/vs/16/release/vs_community.exe"
 set "vsfilename=program-installer.exe"
@@ -82,10 +72,9 @@ if not exist "%vsfilename%" (
 )
 
 REM MUST MUST MUST Prompt user to uncheck "C++ CMake tools for Windows"
+	::currently show user what to check in installation readme. Could be explained clearer
 start /wait vs_community.exe
-
-REM to-do, wait for user to finish installing
-echo Installation and setup completed.
+echo VS Studio 19 install complete.
 
 
 
@@ -93,13 +82,13 @@ REM Install OpenCV
 set "opencvurl=https://github.com/ros2/ros2/releases/download/opencv-archives/opencv-3.4.6-vc16.VS2019.zip”
 set "opencvzipfile=opencv-3.4.6-vc16.VS2019.zip"
 
-curl -o "%opencvzipfile%" "%opencvurl%"
+"%curlPath%" -o "%opencvzipfile%" "%opencvurl%"
 if not exist "%opencvzipfile%" (
     echo Download failed. Exiting...
     exit /b 1
 )
 
-7z x "%opencvzipfile%" -o"%USERPROFILE%\Downloads"
+"%7zipPath%" x "%opencvzipfile%" -o"%USERPROFILE%\Downloads"
 if not exist "%USERPROFILE%\Downloads" (
     echo Extraction failed. Exiting...
     exit /b 1
@@ -130,3 +119,6 @@ curl -O "https://github.com/ros2/choco-packages/releases/download/2022-03-15/tin
 curl -O "https://github.com/ros2/choco-packages/releases/download/2022-03-15/tinyxml2.6.0.0.nupkg"
 
 choco install -y -s %CD% asio cunit eigen tinyxml-usestl tinyxml2 bullet
+
+
+endlocal
